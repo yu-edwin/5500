853a768c88c7e1b0ace7e6a79ea8e2f1a2d6db35
yu-edwin <edwinjcy@gmail.com>
2025-10-09 15:54:40 -0700

    adding base swift project

---
+++ SmartFitTests/SmartFitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartFitTests.swift
+//  SmartFitTests
+//
+//  Created by Edwin Yu on 2025-10-09.
+//
+
+import Testing
+@testable import SmartFit
+
+struct SmartFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
