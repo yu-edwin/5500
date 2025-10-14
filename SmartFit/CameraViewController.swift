73df32bcf35a2f3cb3be4c955e47bdce3edef60d
yu-edwin <edwinjcy@gmail.com>
2025-10-14 02:10:27 -0700

    adding camera view and controller

---
+++ SmartFit/CameraViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CameraViewController.swift
+//  SmartFit
+//
+//  Created by Edwin Yu on 2025-10-14.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraViewController: UIViewController {
+    private let captureSession = AVCaptureSession()
+    private var previewLayer: AVCaptureVideoPreviewLayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCamera()
+    }
+
+    private func setupCamera() {
+        guard let device = AVCaptureDevice.default(for: .video),
+              let input = try? AVCaptureDeviceInput(device: device),
+              captureSession.canAddInput(input) else { return }
+
+        captureSession.addInput(input)
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer?.videoGravity = .resizeAspectFill
+        previewLayer?.frame = view.bounds
+        if let previewLayer = previewLayer {
+            view.layer.addSublayer(previewLayer)
+        }
+        captureSession.startRunning()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        captureSession.stopRunning()
+    }
+}
