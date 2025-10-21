177dff1d1f4ec5e36fbbbf5cc394cf5ecd42b944
dong135790 <dong135790@gmail.com>
2025-10-21 14:35:59 -0700

    Testing Get route for render

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectToDB from "./config/mongoDBConnection.js";
 import seedData from "./config/seedData.js";
+import wardrobeRoutes from "./routes/wardrobeRoutes.js";
 
 dotenv.config();
 
@@ -11,10 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-connectToDB();
+await connectToDB();
 
-seedData();
+await seedData();
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/wardrobe", wardrobeRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Able to run on Port: ${PORT}`));
